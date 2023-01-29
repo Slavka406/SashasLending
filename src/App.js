ec2d400474b5b3c2b77ec978d43c33c8dd25304d
DailyMantova <dailywb16@gmail.com>
2023-01-29 14:30:17 +0100

    add files Modal, Login, signUp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Body from "./Components/Body";
 import Header from "./Components/Header";
+import Login from "./domains/Auth/Login";
+import Modal from "./Components/Modal";
+import SignUp from "./domains/Auth/SignUp";
 
 function App() {
   return (
     <div>
-      <Header />
+      <div>
+        <Header />
 
-      <Body />
+        <Body />
+      </div>
+      <div>
+        <SignUp />
+        <Login />
+        <Modal />
+      </div>
     </div>
   );
 }
